4bf6593d6bd606f7326f42a59639463413fcf232
Gerald Shen <gerald.shen@mail.utoronto.ca>
2019-10-26 13:52:10 -0400

    Adding FlipCard Activity

---
+++ phase1/app/src/main/java/com/example/game/FlipCardActivity.java
@@ -0,0 +1,13 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlipCardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flip_card);
+    }
+}
