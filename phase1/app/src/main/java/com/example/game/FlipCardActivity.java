66a45048882f1246fa81769868c16b0515211441
Gerald Shen <gerald.shen@mail.utoronto.ca>
2019-10-26 14:44:28 -0400

    Updating FlipCard xml and FlipCardActivity File

--- phase1/app/src/main/java/com/example/game/FlipCardActivity.java
+++ phase1/app/src/main/java/com/example/game/FlipCardActivity.java
@@ -2,12 +2,25 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FlipCardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip_card);
+        //
+//        Button myButton = new Button(this);
+//        myButton.setText("Push Me");
+//        LinearLayout ll = (LinearLayout)findViewById(R.id.rootlinearlayout);
+//        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+//        ll.addView(myButton, lp);
+    }
+
+    public void mainBack(View view) {
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(mainActivityIntent);
     }
 }
