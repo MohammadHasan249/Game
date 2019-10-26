6f77b6033a60b650f08f8075767cf98f9c88c278
lamhenr1 <henry.lam@mail.utoronto.ca>
2019-10-26 14:10:07 -0400

    adding arithmetic game Completed

--- phase1/app/src/main/java/com/example/game/MainActivity.java
+++ phase1/app/src/main/java/com/example/game/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent start = new Intent(getApplicationContext(), MathGame.class);
+        startActivity(start);
+
+        // That intent line that creates a Intent object
+        // is a way to transition to another Java + XML file, so as input i put MathGame.class so it transitions to the MathGame.java activity file
+        // So as soon as this app launches, the main activity's onCreate method will be called (aka where this code is located)
+        // then the intent line will transition to the MathGame.java
+
+
     }
 }
