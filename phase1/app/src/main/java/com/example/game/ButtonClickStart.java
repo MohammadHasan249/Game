ded6fd2c6863f757e6ec9c88f206a4254c42ea8c
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2019-10-27 18:15:02 -0400

    Added ButtonClickStart.onCreate and activity_button_click_start.xml

--- phase1/app/src/main/java/com/example/game/ButtonClickStart.java
+++ phase1/app/src/main/java/com/example/game/ButtonClickStart.java
@@ -1,4 +1,14 @@
 package com.example.game;
 
-public class ButtonClickStart {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ButtonClickStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_click_start);
+    }
 }
