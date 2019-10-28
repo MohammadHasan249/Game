42e9900c66956031ca13ec86b41bad98999cfaed
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2019-10-28 13:54:05 -0400

    Added screenwidth and height methods in ButtonClickMain

--- phase1/app/src/main/java/com/example/game/ButtonClickMain.java
+++ phase1/app/src/main/java/com/example/game/ButtonClickMain.java
@@ -1,11 +1,25 @@
 package com.example.game;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ButtonClickMain extends AppCompatActivity {
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenWHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
 
     void goButtonClickResult(View view) {
         Intent goResult = new Intent(getApplicationContext(), ButtonClickResult.class);
