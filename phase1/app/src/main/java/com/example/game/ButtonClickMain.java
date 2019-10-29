8aa0d0e8a675bd07a7c026cd44ee6d09ff981657
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2019-10-29 16:42:16 -0400

    Integrated Timer class into ButtonClickMain

--- phase1/app/src/main/java/com/example/game/ButtonClickMain.java
+++ phase1/app/src/main/java/com/example/game/ButtonClickMain.java
@@ -1,5 +1,6 @@
 package com.example.game;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Point;
@@ -10,16 +11,20 @@ import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.TimerTask;
 
 public class ButtonClickMain extends AppCompatActivity {
 
-    private ArrayList<Button> buttons;
     private Random r = new Random();
+    private int start_time;
+    private Button mainButton;
+    private TextView countDownText;
 
     public static int getScreenWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
@@ -29,29 +34,33 @@ public class ButtonClickMain extends AppCompatActivity {
         return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 
-    public void setButtons(int n) {
-        for (int i = 0; i < n; i++) {
-            Button b = new Button(this);
-            b.setX(r.nextInt(getScreenWidth()));
-            b.setY(r.nextInt(getScreenHeight()));
-            buttons.add(b);
-        }
+    public static Point getDisplaySize(Context context) {
+        Point point = new Point();
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        manager.getDefaultDisplay().getSize(point);
+        return point;
     }
 
     void goButtonClickResult(View view) {
         Intent goResult = new Intent(getApplicationContext(), ButtonClickResult.class);
         startActivity(goResult);
     }
-    public void setButtonRandomPosition(View view){
-        int randomX = new Random().nextInt(getScreenWidth());
-        int randomY = new Random().nextInt(getScreenHeight());
-        view.setX(randomX);
-        view.setY(randomY);
+    public void setButtonRandomPosition(Button button){
+        int randomX = r.nextInt(getDisplaySize(this).x);
+        int randomY = r.nextInt(getDisplaySize(this).y);
+        button.setX(randomX);
+        button.setY(randomY);
+    }
+
+    private void startRandomButton(Button button) {
+       Timer t = new Timer(start_time * 100, mainButton, countDownText);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_click_main);
+        Bundle bundle = getIntent().getExtras();
+        start_time = (int) bundle.get("Time");
     }
 }
