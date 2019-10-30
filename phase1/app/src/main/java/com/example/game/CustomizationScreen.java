b125cefa2490e5f5f7e158c7cf3b1987816738a5
Henry <h.henrylam@gmail.com>
2019-10-30 02:31:31 -0400

    HomePage.btnLogoutFunc seperated it's function into a no parameter helper function becuase this helper function for log out will be re-usable if in future plan to override android back button
    created a customization screen and function to go to that screen when click on New Game button

---
+++ phase1/app/src/main/java/com/example/game/CustomizationScreen.java
@@ -0,0 +1,14 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomizationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customization_screen);
+    }
+}
--- phase1/app/src/main/java/com/example/game/HomePage.java
+++ phase1/app/src/main/java/com/example/game/HomePage.java
@@ -3,6 +3,7 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -11,27 +12,34 @@ import android.widget.TextView;
 public class HomePage extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
-//    String username;
     TextView textViewUsername;
-
     CurrUser user;
 
+    public void btnNewGameFunc(View view){
+        Intent start = new Intent(getApplicationContext(), CustomizationScreen.class);
+        startActivity(start);
+    }
 
-    public void btnLogoutFunc(View view){
+    public void logout(){
+        // Separating this into a new function in case want to override Android back button with logout function
         sharedPreferences.edit().putString("loggedInUsername", "NA").apply();
         finish();
     }
 
+    public void btnLogoutFunc(View view){
+        logout();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        sharedPreferences = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
         user = new CurrUser(this);
         textViewUsername = findViewById(R.id.textViewUsername);
         textViewUsername.setText(user.getUsername());
-        sharedPreferences = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+
 
     }
 }
