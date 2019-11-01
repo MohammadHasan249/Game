b9b0fe3710ab52e72a574f44fb1539d65672f7b2
Henry <h.henrylam@gmail.com>
2019-11-01 15:03:21 -0400

    made both LevelOnCreate constructors public

--- phase1/app/src/main/java/com/example/game/LevelOnCreate.java
+++ phase1/app/src/main/java/com/example/game/LevelOnCreate.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AlertDialog;
 
 public class LevelOnCreate {
 
-    LevelOnCreate(Context context, String instructions){
+    public LevelOnCreate(Context context, String instructions){
         new AlertDialog.Builder(context)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle("Level Instructions")
@@ -18,7 +18,7 @@ public class LevelOnCreate {
     }
 
 
-    LevelOnCreate(Context context, String instructions, final CountDownTimer t){
+    public LevelOnCreate(Context context, String instructions, final CountDownTimer t){
         new AlertDialog.Builder(context)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle("Level Instructions")
