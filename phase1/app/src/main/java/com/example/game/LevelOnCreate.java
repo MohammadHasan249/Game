d9149cb6bd7afdc86c0c6a4c39ab4be4b66212fb
Gerald Shen <gerald.shen@mail.utoronto.ca>
2019-11-01 14:43:13 -0400

    Made OnCreate constructor public for out of package access and implemented it in FlipCardMain

--- phase1/app/src/main/java/com/example/game/LevelOnCreate.java
+++ phase1/app/src/main/java/com/example/game/LevelOnCreate.java
@@ -6,7 +6,7 @@ import android.content.DialogInterface;
 import androidx.appcompat.app.AlertDialog;
 
 public class LevelOnCreate {
-    LevelOnCreate(Context context, String instructions){
+    public LevelOnCreate(Context context, String instructions) {
         new AlertDialog.Builder(context)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle("Level Instructions")
